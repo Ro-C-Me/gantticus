bd27a629f6b4c9636db070d86fc2539278d3d83d
Robert Meyer <rme@hacon.de>
2025-07-30 23:32:00 +0200

    add missing ChartFilter

---
+++ src/app/domain/ChartFilter.ts
@@ -0,0 +1,22 @@
+import { jsonObject, jsonMember } from 'typedjson';
+
+@jsonObject
+export class ChartFilter {
+    @jsonMember
+    taskFilter: string = '';
+    
+    @jsonMember
+    showDownstreamDeps: boolean = false;
+    
+    @jsonMember
+    showUpstreamDeps: boolean = false;
+    
+    @jsonMember
+    showOpenTasks: boolean = true;
+    
+    @jsonMember
+    showInProgressTasks: boolean = true;
+    
+    @jsonMember
+    showDoneTasks: boolean = true;
+}
